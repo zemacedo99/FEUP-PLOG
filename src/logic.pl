716014cf6fcfc95b7afba8de1b9e5c9e3e494069
zemacedo99 <josedantasmacedo@gmail.com>
2020-11-28 18:14:46 +0000

    win condicions

--- src/logic.pl
+++ src/logic.pl
@@ -73,13 +73,41 @@ replace_space(N, Color, [X | Rest], [X | New_rest]):-
 move(RowIndex, SpaceIndex, Color, Board, NewBoard):-
     /*updates board*/
     replace_row(RowIndex, SpaceIndex, Color, Board, NewBoard).
-    
+    /*podemos  é aqui que vamos chamar aqui o check_for_win*/
+    /* acho que o move talvez possa de receber um player tb para sabermos de quem foi a jogada e mandar ao check for win*/
 
 
 
 check_for_win(Board, Player):-
+    check_orange_win(Board,Player). % verificação da ligação laranja-laranja
     /*checks if there is a connection, if so, call game_over to identify winner and display game over menu*/
     
+check_orange_win(Board,Player):-
+    check_row(0,Board,ListOfColors),   % verifica a primeira linha
+    check_normal_row(1,Board,ListOfColors),      % verificação das linhas normais
+    check_row(12,Board,ListOfColors),   % verifica a ultima linha
+    check_complete_path(ListOfColors).  % verificação de casos expeciais 
+    
+
+/*check if the row have the color, in the case orange*/
+check_row(RowIndex,Board,ListOfColors):-        % ListOfColors -> [RowIndex-SpaceIndex,RowIndex-SpaceIndex,etc]
+    getRow(RowIndex,Board,Row).
+    /* se a Row nao tiver a cor de laranja nao é um caso de vitória e deve terminar*/
+    /* se tiver, guardar a posição (rowIndex e spaceIndex ) onde esta(o) a(s) peça(s) cor de laranja numa lista ListOfColors*/
+    /* */
+
+check_normal_row(NormalRowIndex,Board,ListOfColors):-
+    check_row(NormalRowIndex,Board,NewListOfColors),
+    /* se na ListOfColors nao tiver cores na Row anterior nao é um caso de vitória e deve terminar*/
+    /* se tiver,
+        compara a NewListOfColor com a ListOfColors recebida e ve se as a SpaceIndex da ListaOfColors == SpaceIndex da NewListOfColors e ListaOfColors ==  SpaceIndex-1 da NewListOfColors
+        se true faz apend da NewListOfColors com a listofcolors se nao termina
+    */
+    /*o caso do SpaceIndex == 0  ou SpaceIndex == NewListOfColors.length, ve de maneira difernte */
+    NormalRowIndex == 11, % terminar a recrusividade 
+    NextRowIndex is NormalRowIndex + 1, 
+    check_normal_row(NextRowIndex,Board,NewListOfColors).
+
 
 /*Takes the Player and switches to the other one*/
 switch_player(1, NewPlayer):-
